de9e9e1d16cb62785b8e4fe8ea5934af490d6045
Gabriel <gabriel.souza@terceiro.rnp.br>
2023-07-15 16:37:19 -0300

    feat: tentativa de mqtt e adicao de arquivos

--- fse-trabalho3-adnecaiogabriel/main/wifi.c
+++ fse-trabalho3-adnecaiogabriel/main/wifi.c
@@ -25,7 +25,8 @@
 static EventGroupHandle_t s_wifi_event_group;
 
 static int s_retry_num = 0;
-extern xSemaphoreHandle conexaoWifiSemaphore;
+extern SemaphoreHandle_t connectionWifiSemaphore;
+esp_netif_t* netif;
 
 static void event_handler(void* arg, esp_event_base_t event_base,
                                 int32_t event_id, void* event_data)
@@ -46,18 +47,17 @@ static void event_handler(void* arg, esp_event_base_t event_base,
         ESP_LOGI(TAG, "Endereço IP recebido:" IPSTR, IP2STR(&event->ip_info.ip));
         s_retry_num = 0;
         xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
-        xSemaphoreGive(conexaoWifiSemaphore);
+        xSemaphoreGive(connectionWifiSemaphore);
     }
 }
 
 void wifi_start(){
-
     s_wifi_event_group = xEventGroupCreate();
 
     ESP_ERROR_CHECK(esp_netif_init());
 
     ESP_ERROR_CHECK(esp_event_loop_create_default());
-    esp_netif_create_default_wifi_sta();
+    netif = esp_netif_create_default_wifi_sta();
 
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
@@ -97,12 +97,19 @@ void wifi_start(){
         ESP_LOGE(TAG, "UNEXPECTED EVENT");
     }
 
-    //ESP_ERROR_CHECK(esp_event_handler_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler));
-    //ESP_ERROR_CHECK(esp_event_handler_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler));
+    ESP_ERROR_CHECK(esp_event_handler_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler));
+    ESP_ERROR_CHECK(esp_event_handler_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler));
     vEventGroupDelete(s_wifi_event_group);
 }
 
-void wifi_stop()
-{
-    esp_wifi_stop();
-};
+
+void wifi_stop(){
+    esp_err_t err = esp_wifi_stop();
+    ESP_ERROR_CHECK(err);
+    esp_netif_destroy_default_wifi(netif);
+    ESP_ERROR_CHECK(err);
+    err = esp_event_loop_delete_default();
+    ESP_ERROR_CHECK(err);
+    err = esp_wifi_deinit();
+    ESP_ERROR_CHECK(err);
+}
